c5e7f721ac5286b67bfe6e60f39efc67f6e59cb3
Matt Strauss <strausstester@gmail.com>
2022-04-13 15:02:02 -0400

    Added some validation and file upload logic

--- app/routes.py
+++ app/routes.py
@@ -14,9 +14,45 @@ from app import app
 from app.helpers import *
 from app.forms import *
 from app.models import *
+from werkzeug.utils import secure_filename
 
-# Home route
-@app.route('/')
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
+@app.route('/submit', methods=['POST'])
+def upload_file():
+    # get the submitted form data
+    title = request.form.get('title')
+    error = False
+    # check if the post request has the title
+    if title == '':
+        error = True
+        flash('Please provide a title')
+    # check if the post request has the cover file and user
+    # selected an actual file
+    if 'cover' not in request.files:
+        error = True
+        flash('Please provide a book cover')
+    file = request.files['cover']
+    if file.filename == '':
+        error = True
+        flash('Please provide a book cover')
+    if error:
+        return redirect('/')
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template('index.html')
