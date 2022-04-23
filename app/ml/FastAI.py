3e2fc4f673f2ded49584f577d6c0f7e43aacf84c
Asterisk <44215173+casnz1601@users.noreply.github.com>
2022-04-23 18:47:10 -0400

    FastAI.py

---
+++ app/ml/FastAI.py
@@ -0,0 +1,67 @@
+# @author: hare2; Bryce S.;
+
+from fastai.vision import *
+from pathlib import Path
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+
+
+#p_path = Path(r"dataset")
+df = pd.read_csv("modified_category.csv")
+df.head()
+
+img_size = 256
+
+np.random.seed(90)
+data = (ImageList.from_df(df, "./")
+                    .split_by_rand_pct(0.2)
+                    .label_from_df()
+                    .transform(get_transforms(), size= img_size)
+                    .databunch(bs=6)
+                    )
+                    .normalize(imagenet_stats)
+data.show_batch()
+
+############ Create Model ############
+from fastai.metrics import accuracy, Precision, Recall
+learn = cnn_learner(data, models.resnet34, metrics=[accuracy, Precision(average='macro'), Recall(average='macro')], opt_func=optim.SGD)
+
+#categories to predict
+data.classes
+
+#no of categories present
+data.c
+
+#length of training set
+len(data.train_ds)
+
+#length of validation set
+len(data.valid_ds)
+
+############ Train Model ############
+# Build the CNN model with the pretrained resnet34
+# Error rate = 1 - accuracy
+
+#EDIT NUMBER OF EPOCHS, CAN ALSO ADD A LEARNING RATE (e.g. lr = 0.003)
+# Train the model on # epochs of data at the default learning rate
+learn.data.batch_size = 50
+learn.unfreeze()
+
+learn.fit(10, 0.005)
+
+learn.save('res34-stage1')
+
+############ Evaluate Model ############
+interp = ClassificationInterpretation.from_learner(learn)
+
+# confusion matrix: actual on left, predicted on right
+interp.plot_confusion_matrix(figsize=(17, 17))
+
+
+# print out the most mis-classified classes
+interp.most_confused()
+
+# plotting the top losses
+interp.plot_top_losses(6, figsize=(25,25))
